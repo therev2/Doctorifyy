f17038c734a379db872809a2a861b2df01707348
shanu kumawat <shanukumawat01@gmail.com>
2024-03-11 02:55:43 +0530

    added firebase backend for doc signup and login

--- app/src/main/java/com/example/myapplication/doctor_register.java
+++ app/src/main/java/com/example/myapplication/doctor_register.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class doctor_register extends AppCompatActivity {
+
+    EditText signupEmail, signupPassword;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,32 @@ public class doctor_register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signupEmail = findViewById(R.id.email_doc);
+        signupPassword = findViewById(R.id.pass_doc);
+        signupButton = findViewById(R.id.signupdoc);
+
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("doctor");
+
+                String email = signupEmail.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                HelperClass helperClass = new HelperClass(email, password);
+                reference.child(email).setValue(helperClass);
+
+                Toast.makeText(doctor_register.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(doctor_register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
