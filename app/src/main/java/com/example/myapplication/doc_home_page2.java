d0f2c58fd4f13557ab1a9c51303229c2f014ea12
therev2 <revanchanna@gmail.com>
2024-03-15 02:06:40 +0530

    made the doc homere page ui better

--- app/src/main/java/com/example/myapplication/doc_home_page2.java
+++ app/src/main/java/com/example/myapplication/doc_home_page2.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Doc_home_page extends AppCompatActivity {
+public class doc_home_page2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_doc_home_page2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ma), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
