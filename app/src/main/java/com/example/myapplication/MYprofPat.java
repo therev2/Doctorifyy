ee9afdee7a14b4454120d2e713f1f948d0de1fe4
therev2 <revanchanna@gmail.com>
2024-04-06 16:10:20 +0530

    my profile page

--- app/src/main/java/com/example/myapplication/MYprofPat.java
+++ app/src/main/java/com/example/myapplication/MYprofPat.java
@@ -1,12 +1,15 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,11 +20,26 @@ import com.example.myapplication.databinding.ActivityMyprofPatBinding;
 
 public class MYprofPat extends AppCompatActivity {
 
-
+    TextView textView,name_mp,age_mp,add_mp,ms_mp,gender_mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_myprof_pat);
+        name_mp = findViewById(R.id.n1);
+        age_mp = findViewById(R.id.age);
+        add_mp = findViewById(R.id.add);
+        ms_mp = findViewById(R.id.status);
+        gender_mp =findViewById(R.id.gender);
+        textView = findViewById(R.id.edit);
+
+
+        textView.setOnClickListener(v->{
+            Intent intent = new Intent(MYprofPat.this, complete_your_profile.class);
+            startActivity(intent);
+        });
+    }}
 
 //        binding = ActivityMyprofPatBinding.inflate(getLayoutInflater());
 //        setContentView(binding.getRoot());
@@ -47,4 +65,4 @@ public class MYprofPat extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_myprof_pat);
 //        return NavigationUI.navigateUp(navController, appBarConfiguration)
 //                || super.onSupportNavigateUp();
-    }}
+//    }}
