cc1cf197486b4a2b2773f47f84ce1f828db3089d
Shanu-Kumawat <shanukumawat01@gmail.com>
2024-04-07 16:36:16 +0530

    connected my appointments parent side with firebase

--- app/src/main/java/com/example/myapplication/MyAppointmentsPat_Adapter.java
+++ app/src/main/java/com/example/myapplication/MyAppointmentsPat_Adapter.java
@@ -1,4 +1,64 @@
 package com.example.myapplication;
 
-public class MyAppointmentsPat_Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAppointmentsPat_Adapter extends RecyclerView.Adapter<MyAppointmentsPat_Adapter.MyViewHolder_Doc>{
+
+    Context context;
+
+    ArrayList<HelperClass3> appointment_doc;
+
+    public MyAppointmentsPat_Adapter(Context context, ArrayList<HelperClass3> list_doc) {
+        this.context = context;
+        this.appointment_doc = list_doc;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder_Doc onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.recycler_app,parent, false);
+        return new MyViewHolder_Doc(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder_Doc holder, int position) {
+
+        HelperClass3 helperclass = appointment_doc.get(position);
+        holder.doc_name.setText(helperclass.getDoc_email());
+        holder.app_date.setText(helperclass.getDate());
+        holder.app_time.setText(helperclass.getTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return appointment_doc.size();
+    }
+
+
+
+    public static class MyViewHolder_Doc extends RecyclerView.ViewHolder{
+
+        TextView doc_name, app_date, app_time;
+
+        public MyViewHolder_Doc(@NonNull View itemView) {
+            super(itemView);
+
+            doc_name = itemView.findViewById(R.id.docName);
+            app_time = itemView.findViewById(R.id.time);
+            app_date = itemView.findViewById(R.id.date);
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication/my_app_list.java
+++ app/src/main/java/com/example/myapplication/my_app_list.java
@@ -1,22 +1,35 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.databinding.ActivityMyAppListBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class my_app_list extends AppCompatActivity {
+    RecyclerView recyclerView_appointment;
+    DatabaseReference database;
+    MyAppointmentsPat_Adapter myadapter_appointments;
+    ArrayList<HelperClass3> appointment_list;
+    public static final String SHARED_PREFS="sharedPrefs";
+
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMyAppListBinding binding;
@@ -26,6 +39,42 @@ public class my_app_list extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_my_app_list);
+
+        //getting doc email from shared preference and storing it in variable
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String Email_of_pat = sharedPreferences.getString("patient_email","");
+
+
+        //recycler to get all appointment list
+        recyclerView_appointment = findViewById(R.id.mainuser);
+        database = FirebaseDatabase.getInstance().getReference("appointment");
+        recyclerView_appointment.setHasFixedSize(true);
+        recyclerView_appointment.setLayoutManager(new LinearLayoutManager(this));
+
+        appointment_list = new ArrayList<>();
+        myadapter_appointments = new MyAppointmentsPat_Adapter(this,appointment_list);
+        recyclerView_appointment.setAdapter(myadapter_appointments);
+
+        //getting all appointments from database
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                appointment_list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    HelperClass3 helper = dataSnapshot.getValue(HelperClass3.class);
+                    if (helper.getPat_email().toLowerCase().contains(Email_of_pat.toLowerCase())) {
+                        appointment_list.add(helper);
+                    }
+                }
+                //refreshing for data change
+                myadapter_appointments.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 //        binding = ActivityMyAppListBinding.inflate(getLayoutInflater());
